25da9eb1121b2cb9a9a9ee30af3236c4e4a6266d
Aj-code-17 <Shontyfly@gmail.com>
2025-02-17 22:53:14 +0500

    Update script.js

--- Public/script.js
+++ Public/script.js
@@ -1,3 +1,6 @@
 // Create the 3D globe
    const globe = new WorldWind.WorldWindow("globe");
    globe.addLayer(new WorldWind.BMNGLayer()); // Add Earth texture
+// Create the 2D map
+   const map = L.map('map').setView([0, 0], 2);
+   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
