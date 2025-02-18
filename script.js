34bcd6c364e6de4a576c0a44924c34131a78fd13
Aj-code-17 <Shontyfly@gmail.com>
2025-02-18 11:05:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ globe.addLayer(new WorldWind.CompassLayer()); // Add compass
 // Initialize 2D Map
 const map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-
+async function fetchISS() {
+  const proxy = "https://corsproxy.io/?";
+  const response = await fetch(proxy + 'https://api.wheretheiss.at/v1/satellites/25544');
+  const data = await response.json();
+  return { lat: data.latitude, lon: data.longitude };
+}
 // Test ISS marker (fixed position)
 const issPlacemark = new WorldWind.Placemark(
   new WorldWind.Position(0, 0, 400000),
